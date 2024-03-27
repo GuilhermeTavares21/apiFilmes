1a70caa87f772a75f2811d14c78c0d3c7eb0bf93
GuilhermeTavares21 <guitprates@gmail.com>
2024-03-27 19:27:57 -0300

    first commit

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,33 @@
+﻿using FilmesApiAlura.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApiAlura.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : Controller
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    [HttpPost]
+    public void Adicionar([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> GetFilmes()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? GetFilmeById(int id)
+    {
+        return filmes.FirstOrDefault(filmes => filmes.Id == id);
+    }
+
+
---
+++ Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApiAlura.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50,ErrorMessage = "Número máximo de 50 caracteres")]
+    public string Titulo { get; set; }
+    [Required]
+    [MaxLength(12, ErrorMessage = "Número máximo de 12 caracteres")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(60,600, ErrorMessage ="A duração deverá ser entre 60 a 600 minutos")]
+    public int Duracao { get; set; }
+    
+}
